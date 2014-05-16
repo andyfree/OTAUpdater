13e3e8b0fbbef66ec42a6ced1b48663bad492a3f
Kryten2k35 <kryten2k35@gmail.com>
2014-05-16 14:57:43 +0100

    Corrected Makefile
    Fixed the Makefile to correctly use the appcompat library

--- Android.mk
+++ Android.mk
@@ -1,6 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+# Include res dir from appcompat
+appcompat_dir := ../../../frameworks/support/v7/appcompat/res
+res_dirs := $(appcompat_dir) res
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
@@ -10,6 +14,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_PACKAGE_NAME := OTAUpdater
 LOCAL_CERTIFICATE := shared
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 include $(BUILD_PACKAGE)
 
